406c82e14e05542a0558c4a3abf4f592e4eed779
alexL <aleksandrlambert@gmail.com>
2024-03-21 14:46:59 +0700

    выполнили первые три шага

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
-// создаем imput
+// создаем input
 // добавляем класс
 // добавляем placeholder
-// убрать/сбросить стандартное поведение imput
+// убрать/сбросить стандартное поведение input
 
 // интегрировать обработчик по клику
 
 // создать кнопку "скопировать"
 // создать кнопку "сгенерировать"
 // создать функцию генерации пароля
+
+const mainEl = document.querySelector(".main");
+const passwordEl = document.createElement(".input");
+passwordEl.classList.add(".password");
+passwordEl.setAttribute("placeholder", "Сгенерировать пароль");
+
